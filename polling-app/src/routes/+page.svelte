9ec23db3ea7b51a9f612b8a86ca04622b66b19db
Frexz <fredrik.pettersen3@gmail.com>
2024-06-18 21:14:57 +0200

    each

--- polling-app/src/routes/+page.svelte
+++ polling-app/src/routes/+page.svelte
@@ -1,9 +1,32 @@
 <script>
-    
+    let people = [
+        { name: 'Yoshi', beltColor: 'green', age: 25, id: 1},
+        { name: 'Mario', beltColor: 'orange', age: 45, id: 2},
+        { name: 'Luigi', beltColor: 'black', age: 35, id: 3},
+    ]    
 </script>
 
-
+<main>
+    {#each people as person (person.id)}
+        <div class="person">
+            <h4>{person.name}</h4>
+            <p>{person.age} years old, {person.beltColor} belt.</p>
+        </div>
+    {:else}
+        <p>There are no people to show ...</p>
+    {/each}
+</main>
 
 <style>
-    
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center
+    }
+
+    .person {
+        display: flex;
+        flex-direction: column;
+    }
 </style>
