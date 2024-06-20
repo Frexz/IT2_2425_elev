8ee7987f37c2ab62c76006e6cdcfbdc745c0c7a7
Frexz <fredrik.pettersen3@gmail.com>
2024-06-20 22:01:01 +0200

    added delete button to polling project

--- polling-app/src/routes/+page.svelte
+++ polling-app/src/routes/+page.svelte
@@ -3,7 +3,11 @@
         { name: 'Yoshi', beltColor: 'green', age: 25, id: 1},
         { name: 'Mario', beltColor: 'orange', age: 45, id: 2},
         { name: 'Luigi', beltColor: 'black', age: 35, id: 3},
-    ]    
+    ]
+
+    const handleClick = (id) => {
+        people = people.filter((person) => person.id != id)
+    }
 </script>
 
 <main>
@@ -11,6 +15,7 @@
         <div class="person">
             <h4>{person.name}</h4>
             <p>{person.age} years old, {person.beltColor} belt.</p>
+            <button on:click={() => handleClick(person.id)}>Delete</button>
         </div>
     {:else}
         <p>There are no people to show ...</p>
