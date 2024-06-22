ab31f3d48d1666a28883869d7ab3c136172f715c
Frexz <fredrik.pettersen3@gmail.com>
2024-06-22 23:25:18 +0200

    added a tabs system

--- polling-app/src/routes/+page.svelte
+++ polling-app/src/routes/+page.svelte
@@ -1,11 +1,24 @@
 <script>
     import Header from '../components/Header.svelte'
     import Footer from '../components/Footer.svelte'
+    import Tabs from '../shared/Tabs.svelte'
+
+    let items = ['Current Polls', 'Add New Poll']
+    let activeItem = 'Current Polls'
+
+    const handleTabChange = (e) => {
+        activeItem = e.detail
+    }
 </script>
 
 <Header />
 <main>
-    
+    <Tabs {items} {activeItem} on:tabChange={handleTabChange} />
+    {#if activeItem === 'Current Polls'}
+        <p>Poll list component goes here</p>
+    {:else if activeItem === 'Add New Poll'}
+        <p>New poll form component goes here</p>
+    {/if}
 </main>
 <Footer />
 
@@ -17,6 +30,5 @@
 
     :global(body) {
         margin: 0;
-
     }
 </style>
