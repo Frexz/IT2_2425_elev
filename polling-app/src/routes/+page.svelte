81ca54a9b5f9285026aaa42b1df13e26ac13796b
Frexz <fredrik.pettersen3@gmail.com>
2024-06-22 22:55:18 +0200

    created polling project

---
+++ polling-app/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    
+</script>
+
+<main>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nec posuere tortor. Quisque et lectus elit. Suspendisse consequat orci neque, vel cursus lacus vestibulum quis. Aenean ut egestas eros. Praesent vitae odio sed tellus porta sagittis sit amet sit amet mauris. Maecenas mi massa, hendrerit sit amet rhoncus volutpat, pharetra in dui. Morbi sed commodo diam, eget ultrices enim. Suspendisse placerat, mauris gravida sollicitudin semper, magna tortor accumsan mi, ac semper turpis tellus eu arcu. Cras mattis eleifend mauris vitae rhoncus. Quisque auctor lobortis leo, in consequat massa pretium eget.</p>
+</main>
+
+<style>
+    main {
+        max-width: 960px;
+        margin: 40px auto;
+    }
+</style>
