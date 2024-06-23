a16f4bcc212bcb83764b0481c34cd54ff74249eb
Frexz <fredrik.pettersen3@gmail.com>
2024-06-24 01:10:16 +0200

    added routes and links

--- gaming-website/src/routes/+page.svelte
+++ gaming-website/src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    
+</script>
+
+<div class="home">
+    <h2>Welcome</h2>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem recusandae magnam a consequatur debitis sequi, laudantium modi dignissimos commodi quod magni odio nulla amet consectetur atque? Laborum delectus dolorem rem.
+    </p>
+    <a href="/guides">View Guides</a>
+    <a href="/about">About</a>
+</div>
+
+<style>
+    .home {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
