ff0369d58d1d00f102529dfcd51d1f9b5d1f82b0
Frexz <fredrik.pettersen3@gmail.com>
2024-10-28 08:00:49 +0100

    oop

--- runde_2/2.4_funksjoner/undervisning.py
+++ runde_2/2.4_funksjoner/undervisning.py
@@ -1,19 +1,7 @@
-def regn_ut(a: float, b: float) -> tuple[float, float, float, float]:
-    sum_ = a + b
-    diff = a - b
-    prod = a * b
-    try:
-        kvot = round(a / b, 3)
-    except ZeroDivisionError:
-        kvot = 'ugyldig'
-    return sum_, diff, prod, kvot
+def get_initials(name):
+    return name[0]
 
-assert regn_ut(2, 3) == (5, -1, 6, 0.667), f"Feil, fikk {regn_ut(2, 3)} i stedet."
-assert regn_ut(5, 0) == (5, 5, 0, 'ugyldig')
-assert regn_ut(-4, -3) == (-7, -1, 12, 1.333)
+first_name = input("What is your name? ")
+initials = get_initials(first_name)
 
-print("Funksjonen bestod testen!")
-
-
-
-
+print(f"Your name is {first_name} and your initials are {initials}.")
